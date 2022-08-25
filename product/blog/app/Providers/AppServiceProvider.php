310e66aef05c0ca8ee30b8c651a368f74d0ade9e
mzq <578377590@qq.com>
2022-08-26 01:07:55 +0800

    测试

---
+++ product/blog/app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Db::listen(function ($query){
+            dd($query->sql, $query->time);
+        });
+
+        Schema::defaultStringLength(200);
+    }
+}
